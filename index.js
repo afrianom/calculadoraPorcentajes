fff3abbba6b84ec1ce708bec2cd97a0b37356515
piperiao <andresfeliperianom@gmail.com>
2024-07-03 21:16:18 -0500

    add a new kind of percentage

--- index.js
+++ index.js
@@ -28,3 +28,19 @@ document.getElementById('percentage-form2').addEventListener('submit', function(
     const result2 = total2 - percentageValue;
     document.getElementById('result-rest').textContent = `El total después de restar el ${percentage2}% de ${total2} es ${result2}`;
 });
+
+document.getElementById('percentage-form3').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const total3 = parseFloat(document.getElementById('total-oc').value);
+    const percentage3 = parseFloat(document.getElementById('percentage-oc').value);
+
+    if (isNaN(total3) || isNaN(percentage3)) {
+        alert('Por favor, introduce valores válidos.');
+        return;
+    }
+
+    const percentageValue2 = (100 * percentage3) / total3;
+    const result3 = percentageValue2;
+    document.getElementById('result-oc').textContent = `Si tienes ${percentage3} PODS ocupados de ${total3}, el porcentage de ocupación es ${result3}%`;
+});
